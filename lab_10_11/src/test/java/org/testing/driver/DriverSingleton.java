a6e1e62fbcf562fc4f9900c38e1abd368aa98a19
KotSlava <kot.slava.mih@gmail.com>
2021-12-14 04:57:51 +0300

    update

--- lab_10_11/src/test/java/org/testing/driver/DriverSingleton.java
+++ lab_10_11/src/test/java/org/testing/driver/DriverSingleton.java
@@ -3,6 +3,7 @@ package org.testing.driver;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class DriverSingleton {
@@ -17,9 +18,12 @@ public class DriverSingleton {
                 WebDriverManager.firefoxdriver().setup();
                 driver = new FirefoxDriver();
             }
+            ChromeOptions options = new ChromeOptions();
+            options.addArguments("--headless", "--no-sandbox", "--disable-dev-shm-usage", "--window-size=1920,1080",
+                    "--disable-extensions", "--proxy-server='direct://'", "--proxy-bypass-list=*", "--start-maximized",
+                    "--disable-gpu", "--ignore-certificate-errors");
             WebDriverManager.chromedriver().setup();
             driver = new ChromeDriver();
-            driver.manage().window().maximize();
         }
         return driver;
     }
